283b1871fca7e8cf54115d555a91541574892e10
squid233 <513508220@qq.com>
2020-10-31 18:34:26 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Minecraft2D-Kotlin
+This Minecraft2D Kotlin Version. You can see the Java Version [here](https://github.com/Over-Run/Minecraft2D).
---
+++ src/main/kotlin/io/github/overrun/mc2d/Minecraft2D.kt
@@ -0,0 +1,35 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Over-Run
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.github.overrun.mc2d
+
+import io.github.overrun.mc2d.logger.DefaultLogger
+import io.github.overrun.mc2d.logger.LogManager
+
+object Minecraft2D {
+    const val VERSION = "0.1.0-pre-alpha"
+    const val NAMESPACE = "minecraft2d"
+
+    fun getLogger(clazz: Class<*>): DefaultLogger = LogManager.getLogger("minecraft2d/${clazz.simpleName}")
+}
